e9f6d98a0140ff2651295577c403fc8a7e44bb20
Naman Pandey <2492004namanpandey@gmail.com>
2025-03-24 09:03:35 +0530

    Fixed Vercel 404 issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
   server: {
-    historyApiFallback: true, // ✅ Ensures React Router works
+    historyApiFallback: true,
   },
+  publicDir: "public",
 });
